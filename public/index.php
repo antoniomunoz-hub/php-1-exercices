a74035dfc6af077fcb178941babc7b84d5cccc6a
Antonio Muñoz <antonio.bjjrga@gmail.com>
2021-06-02 13:54:31 +0200

    php

---
+++ public/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <h1>Kobra Kai</h1>
+    <p>Strike First Strike Strong No Mercy!!</p>
+</body>
+</html>
+
+
+// $var1='hola mundo'; 
+// $var2='adios cupido';
+// $var3=false;
+// $var4=null;
+// $var5=5;
+// $var6=[1, 3];
+
+// print_r($var1."<br><br>");
+// print_r($var2."<br><br>");
+// print_r($var3."<br><br>");
+// print_r($var4."<br><br>");
+// print_r($var5."<br><br>");
+// print_r($var6."<br><br>");
