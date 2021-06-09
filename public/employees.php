a76fb76b3ab27582a4176db7f3796c625550bf13
Antonio Muñoz <antonio.bjjrga@gmail.com>
2021-06-09 13:12:34 +0200

    post

--- public/employees.php
+++ public/employees.php
@@ -1,7 +1,23 @@
 <?php
     require $_SERVER['DOCUMENT_ROOT'] . '/lib/app.php';
 
-    $stm = $dbConnexion->query('SELECT * FROM employees;');
+    $query = 'SELECT * FROM employees';
+    if (isset($_GET['id'])) {
+        $query = 'SELECT * FROM employees WHERE id = :identifiador';
+    } elseif (isset($_GET['email'])) {
+        $query = 'SELECT * FROM employees WHERE email = :correo';
+    }
+    
+    $stm = $dbConnexion->prepare($query);
+
+    if (isset($_GET['id'])) {
+        $stm->bindParam(':identificador', $_GET['id']);
+    } elseif (isset($_GET['email'])) {
+        $stm->bindParam(':correo', $_GET['email']);
+    }
+
+    $stm->execute();
+
     $people = $stm->fetchAll(PDO::FETCH_ASSOC);
 ?>
 <?php
--- public/employees_html.php
+++ public/employees_html.php
@@ -24,6 +24,24 @@
             <?php endforeach; ?>
         </tbody>
     </table>
+
+    <hr/>
+    <form method="POST" action="">
+        <label for="name">Nombre</label>
+        <input type="text" id="name" name="name" required/>
+        <br/>
+        <label for="email">Email</label>
+        <input type="email" id="email" name="email" required/>
+        <br/>
+        <label for="age">Edad</label>
+        <input type="number" id="age" name="age" required/>
+        <br/>
+        <label for="city">Ciudad</label>
+        <input type="text" id="city" name="city" />
+        <hr/>
+        <input type="submit" value="Enviar"/>
+    </form>
+
     <?php require 'partial/footer.php'; ?>
 </body>
 
